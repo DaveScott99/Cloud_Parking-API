012655df2f5d471a19f790c82ac57ec06091edef
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-11-08 17:18:51 -0300

    Primeiro endpoint de lista

--- src/main/java/com/astro/parking/Controller/HelloController.java
+++ src/main/java/com/astro/parking/Controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.astro.parking.Controller;
+package com.astro.parking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
